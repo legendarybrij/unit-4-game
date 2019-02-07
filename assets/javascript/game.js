515a5e9b4b1af24c5d1134d1d6ffeb704d348cc2
Brijesh Patel <Brij@Brijesh-Patels-MacBook-Pro.local>
2019-02-06 23:13:05 -0500

    made css

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+
+
+// 1) Display four thumbmail of Star Wars Characters in document.ready function
+/* 2) When user selects one character then that thumbmail comes below Your Character 
+      and others below Enemies Available To Attack */
+//2.5) If user click attack before selecting opponent then it displays "No enemy here" below Defender      
+// 3) User select one character from Enemies Available to Attack which moves below Defender
+// 4) When user click attack then it randomly decreases health of opponent as follows
+//      "You attacked "opponentName" for 8 damage" below defendent 
+//      "opponentName" attacked you back for 25 damage"
+// 6) Repeat Step 4 until one of the character looses. Also AttackPower = AttackPower + 8; each time your charcter attacks.
+// 7) If Your Character's health becomes zero first then Game Over
+// 8) Else if opponent's health becaomes zero then the thumbmail disappears
+// 9) Now Your Character can attack 2 more remaining character from Enemies Available To Attack
+//10) Your character selects one more from 2 remaining character from Enemies Available To Attack
+//11) Repeat Step 4 until one of the character looses.
+//12) Now Your Character can attack 1 more remaining character from Enemies Available To Attack
+//13) Your character selects last remaining character from Enemies Available To Attack
+//14) Repeat Step 4 until one of the character looses.
+//15) If your character's health remains above zero then wins & Game Over
+
+
