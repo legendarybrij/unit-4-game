77a34bf9d2184aed0be7dd27b6b1d0899e2b0a3c
Brijesh Patel <Brij@Brijesh-Patels-MacBook-Pro.local>
2019-02-11 22:40:56 -0500

    complete logics behind game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,115 +20,264 @@
 //14) Repeat Step 4 until one of the character looses.
 //15) If your character's health remains above zero then wins & Game Over
 
-var clickedYour = "";
+    var hero = "";
     var enemy = "";
     var darthHp = Math.floor((Math.random())*100) + 102;
     var lukeHp  = Math.floor((Math.random())*100) + 102;
     var yodaHp  = Math.floor((Math.random())*100) + 102;
     var hanHp   = Math.floor((Math.random())*100) + 102;
+    var darth = "Darth Vader";
+    var luke = "Luke Skywalker";
+    var yoda = "Yoda";
+    var han = "Han Solo";
 
+    var yourAttack = 0;
+    var enemyAttack = 0;
+    var yourHealth  = 1;
+    var enemyHealth = 1;
+ 
 $(document).ready(function(){
-    
-console.log("Darth = "+darthHp+ " Luke = "+lukeHp+" Yoda = "+yodaHp+ " Han = "+hanHp)
-    $("#darth").on("click", function() {
+    yourAttack = Math.floor((Math.random())* 20) + 10;
+    enemyAttack = Math.floor((Math.random())* 20) + 20;
+    $(".enemy1").hide(); $(".enemy2").hide(); $(".enemy3").hide(); $(".myHero").hide();$(".currentEnemy").hide();
+console.log("Darth = "+darthHp+ " Luke = "+lukeHp+" Yoda = "+yodaHp+ " Han = "+hanHp);
+console.log("YourAttack = "+yourAttack + " EnemyAttack = "+enemyAttack);
 
-        $("#darth").clone().appendTo( ".current" );
-        $("#darth").hide();
+    document.getElementById("darthHp").innerHTML = darthHp;
+    document.getElementById("lukeHp").innerHTML = lukeHp;
+    document.getElementById("yodaHp").innerHTML = yodaHp;
+    document.getElementById("hanHp").innerHTML = hanHp;
+
+    $("#darth").on("click", function() {
+        
+        $("#darth").clone().appendTo( ".currentHero" );
+       $("#darth").hide();
+        $(".myHero").show();
+        $(".enemy1").show(); $(".enemy2").show(); $(".enemy3").show();
         $("#luke").clone().appendTo( ".enemy1" );
         $("#yoda").clone().appendTo( ".enemy2" );
         $("#han").clone().appendTo( ".enemy3" );
-        $("#allCharacters").hide();
-        clickedYour="darth";
+       $("#allCharacters").hide();
+        hero=darth;
+        
+        yourHealth =  darthHp;
+        document.getElementById("darthHp").innerHTML = yourHealth;
+       
+        //console.log(darthHp);
+       // document.getElementById("darthHp").innerHTML = yourHealth;
+        
       });     
     
       $("#luke").on("click", function() {
         
-        $("#luke").clone().appendTo( ".current" );
+        $("#luke").clone().appendTo( ".currentHero" );
         $("#luke").hide();
+        $(".myHero").show();
+        $(".enemy1").show(); $(".enemy2").show(); $(".enemy3").show();
         $("#darth").clone().appendTo( ".enemy1" );
         $("#yoda").clone().appendTo( ".enemy2" );
         $("#han").clone().appendTo( ".enemy3" );
         $("#allCharacters").hide();
-        clickedYour="luke";
+        hero=luke;
+        yourHealth = lukeHp;
+        //document.getElementById("lukeHp").innerHTML = yourHealth;
       }); 
 
       $("#yoda").on("click", function() {
         
-        $("#yoda").clone().appendTo( ".current" );
+        $("#yoda").clone().appendTo( ".currentHero" );
         $("#yoda").hide();
+        $(".myHero").show();
+        $(".enemy1").show(); $(".enemy2").show(); $(".enemy3").show();
         $("#darth").clone().appendTo( ".enemy1" );
         $("#luke").clone().appendTo( ".enemy2" );
         $("#han").clone().appendTo( ".enemy3" );
         $("#allCharacters").hide();
-        clickedYour="yoda";
+        hero=yoda;
+        yourHealth = yodaHp;
+        //document.getElementById("yodaHp").innerHTML = yourHealth;
       }); 
 
       
 
       $("#han").on("click", function() {
         
-        $("#han").clone().appendTo( ".current" );
+        $("#han").clone().appendTo( ".currentHero" );
         $("#han").hide();
+        $(".myHero").show();
+        $(".enemy1").show(); $(".enemy2").show(); $(".enemy3").show();
         $("#darth").clone().appendTo( ".enemy1" );
         $("#luke").clone().appendTo( ".enemy2" );
         $("#yoda").clone().appendTo( ".enemy3" );
         $("#allCharacters").hide();
-        clickedYour="han";
+        hero=han;
+        yourHealth = hanHp;
+       // document.getElementById("hanHp").innerHTML = yourHealth;
       }); 
 
-
-      $(".enemy1").on("click", function() {
-        if(clickedYour==="darth")
-        {
-            $("#luke").clone().appendTo(".defender");
-            $(".enemy1").hide();
-            enemy = "luke"
-        }else if(clickedYour==="luke" || clickedYour==="yoda" || clickedYour==="han")
-        {
-            $("#darth").clone().appendTo(".defender");
-            $(".enemy1").hide();
-            enemy = "darth"
-        }
-      });
-
-
-      $(".enemy2").on("click", function() {
-        if(clickedYour==="darth" || clickedYour==="luke")
-        {
-            $("#yoda").clone().appendTo(".defender");
-            $(".enemy2").hide();
-            enemy = "yoda"
-        }else if (clickedYour==="yoda"|| clickedYour==="han")  
-        {
-            $("#luke").clone().appendTo(".defender");
-            $(".enemy2").hide();
-            enemy = "luke"
-        }
-
-      });
-
-      $(".enemy3").on("click", function() {
-        if(clickedYour==="darth" || clickedYour==="luke" || clickedYour==="yoda")
-        {
-            $("#han").clone().appendTo(".defender");
-            $(".enemy3").hide();
-            enemy = "han"
-        }else if ( clickedYour==="han")  
-        {
-            $("#yoda").clone().appendTo(".defender");
-            $(".enemy3").hide();
-            enemy = "yoda"
-        }
-
-      });
-     
       
+            $(".enemy1").on("click", function() {
+                if(hero===darth && $(".currentEnemy").find('img').length===0)
+                {
+                    $("#luke").clone().appendTo(".currentEnemy").addClass("img-thumbnail-enemy");
+                    $(".enemy1").remove();
+                    $(".currentEnemy").show();
+                    $(".score").empty();
+                    //$("#enemy").remove();
+                    enemy = luke;
+                    enemyHealth = lukeHp;
+                    //console.log("img =" + $(".currentEnemy").find('img').length);
+                    
+                }else if((hero===luke || hero===yoda || hero===han) && $(".currentEnemy").find('img').length===0)
+                {
+                    $("#darth").clone().appendTo(".currentEnemy");
+                    $(".enemy1").remove();
+                    $(".currentEnemy").show();
+                    $(".score").empty();
+                    enemy = darth;
+                    enemyHealth = darthHp;
+                   
+                }
+            });
+
+            
+            $(".enemy2").on("click", function() {
+                if((hero===darth || hero===luke)&& $(".currentEnemy").find('img').length===0)
+                {
+                    $("#yoda").clone().appendTo(".currentEnemy");
+                    $(".enemy2").remove();
+                    $(".currentEnemy").show();
+                    $(".score").empty();
+                    enemy = yoda;
+                    enemyHealth = yodaHp;
+                }else if ((hero===yoda|| hero===han)&& $(".currentEnemy").find('img').length===0)  
+                {
+                    $("#luke").clone().appendTo(".currentEnemy");
+                    $(".enemy2").remove();
+                    $(".currentEnemy").show();
+                    $(".score").empty();
+                    enemy = luke;
+                    enemyHealth = lukeHp;
+                }
+
+            });
+            
+
+            $(".enemy3").on("click", function() {
+                if((hero===darth || hero===luke || hero===yoda) && $(".currentEnemy").find('img').length===0)
+                {
+                    $("#han").clone().appendTo(".currentEnemy");
+                    $(".enemy3").remove();
+                    $(".currentEnemy").show();
+                    $(".score").empty();
+                    enemy = han;
+                    enemyHealth = hanHp;
+                }else if ( (hero===han) && $(".currentEnemy").find('img').length===0)  
+                {
+                    $("#yoda").clone().appendTo(".currentEnemy");
+                    $(".enemy3").remove();
+                    $(".currentEnemy").show();
+                    $(".score").empty();
+                    enemy = yoda;
+                    enemyHealth = yodaHp;
+                }        
+
+             });
+
+             
+                $("#attack").on("click",function(){
+                    
+                    if($(".currentEnemy").find('img').length===0 && $(".currentHero").find('img').length===0 )
+                    {
+                        $(".score").text("Please Select Your Chracter and Opponent Before Attacking");
+                    }else if ($(".allEnemy").find('img').length===0 && $(".currentEnemy").find('img').length===0 )
+                    {
+                        $(".score").text("You have cleared the arena hence you are the true Star!!");
+                    }else if ($(".currentEnemy").find('img').length===0)
+                    {
+                        $(".score").text("Please Select Your Opponent Before Attacking");
+                    }else if (yourHealth>=0 && enemyHealth>0)
+                    {
+                        if(hero===darth) { $("#darth").text(yourHealth); } else if(hero===luke){ $("#luke").text(yourHealth); } else if(hero===yoda){ $("#yoda").text(yourHealth); } else if(hero===han){ $("#han").text(yourHealth); }
+                        if(enemy===darth){ $("#darth").text(enemyHealth); } else if(enemy===luke) { $("#luke").text(enemyHealth); } else if(enemy===yoda){ $("#yoda").text(enemyHealth); } else if(enemy===han){ $("#han").text(enemyHealth); }
+                       
+                        //$("#darth").append(yourHealth);
+                       // $("#darthHp").append("#hero");
+                        yourHealth -= enemyAttack;
+                        enemyHealth -= yourAttack;
+                        yourAttack = yourAttack + 30;
+                        $(".score").text("You attacked "+ enemy+" for "+yourAttack+" Damage");
+                        $("<div>").appendTo(".score").text(enemy+" attacked you back for "+enemyAttack+" Damage");
+                        console.log("yourHealth = "+yourHealth+" && enemyHealth = "+enemyHealth);
+                       
+                        //document.getElementById("darthHp").innerHTML = yourHealth;
+                       // document.getElementById("lukeHp").innerHTML = enemyHealth;
+                        if(hero===darth)
+                        {
+                            document.getElementById("darthHp").innerHTML = yourHealth;
+                        }else if (hero===luke)
+                        {
+                            document.getElementById("lukeHp").innerHTML = yourHealth;
+                        }else if (hero===yoda)
+                        {
+                            document.getElementById("yodaHp").innerHTML = yourHealth;
+                        }else if (hero===han)
+                        {
+                            document.getElementById("hanHp").innerHTML = yourHealth;
+                        }                 
+                                if(enemy===darth)
+                                {
+                                    document.getElementById("darthHp").innerHTML = enemyHealth;
+                                }else if (enemy===luke)
+                                {
+                                    document.getElementById("lukeHp").innerHTML = enemyHealth;
+                                }else if (enemy===yoda)
+                                {
+                                    document.getElementById("yodaHp").innerHTML = enemyHealth;
+                                }else if (enemy===han)
+                                {
+                                    document.getElementById("hanHp").innerHTML = enemyHealth;
+                                }     
+
+                        
+                    }else if (yourHealth<=0 && enemyHealth>0)
+                    {
+                        $(".score").text("You Lose");
+
+                    }else if (enemyHealth<=0 && yourHealth>0)
+                    {
+                        $(".score").text("You Win");
+                        $(".currentEnemy").empty();
+                        $(".currentEnemy").hide();
+                        enemyAttack = Math.floor((Math.random())* 20) + 20;
+                    }else if (enemyHealth<=0 && yourHealth<=0)
+                    {
+                        $(".score").text("You both are dead. Match is Draw");
+                       // $(".currentEnemy").empty();
+                        //$(".currentEnemy").hide();          
+                    }
+
+                });
+              
+      
+      //$(".currentEnemy1").remove();
+      console.log("img =" + $(".currentEnemy").find('img').length);
+      /*$(".currentEnemy1").hide();
+      $(".currentEnemy2").hide();
+      $(".currentEnemy3").hide();*/
 });
 
-var game={ 
-        health: 0,
+/*var game={ 
+       yourHp: yourHealth,
+       enemyHp: enemyHealth,
 
+       attack:function(){
+
+       },
         
     
+    
 
-    };
+       
+
+    };*/
